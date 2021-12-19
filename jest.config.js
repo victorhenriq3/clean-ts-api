a2aecfd822dd3e7aed87c5bea26b34b7a4357967
Victor Henrique <vic.1601@outlook.com>
2021-12-19 16:03:42 -0300

    feat: ensure email validator adpater returns fasle if validaotr returns false

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
