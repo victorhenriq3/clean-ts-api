a59d35a7fc7b45abc852948a9b40cf7af13e7a22
Victor Henrique <vic.1601@outlook.com>
2021-12-23 10:49:15 -0300

    feat: ensure accountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAcountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAcountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.insertOne(accountData)
+    return MongoHelper.map(accountData)
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -8,5 +8,17 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (data: any): any {
+    const { _id, ...rest } = data
+    return {
+      ...rest,
+      id: _id
+    }
   }
 }
