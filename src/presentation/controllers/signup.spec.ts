9531de124f51c4b106dc8c1082c9024016895c7f
victorhenriqc <vic.1601@outlook.com>
2023-02-24 17:53:29 -0300

    test: ensure SignupController return 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from './signup'
-import {MissingParamError} from "../errors/missing-param-error"
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('Signup Controller', function () {
   test('Should return 400 if no name is provided', () => {
@@ -27,5 +27,18 @@ describe('Signup Controller', function () {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+  test('Should return 400 if no email is password', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
