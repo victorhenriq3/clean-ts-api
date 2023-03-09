0c1ea56e8d144602b46f0e15b956134cf4d96012
victorhenriqc <vic.1601@outlook.com>
2023-03-09 10:53:19 -0300

    feat: ensure request parse body as json

---
+++ src/main/config/app.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import setUpMiddlewares from './middlewares'
+
+const app = express()
+setUpMiddlewares(app)
+export default app
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,5 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
 app.listen(5050, () => {
     console.log('server running at http://localhost:5050');
-    
 })
