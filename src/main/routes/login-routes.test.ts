ca1afc024e800d5bddcede6e01091a340231a8f1
victorhenriqc <vic.1601@outlook.com>
2023-03-15 14:58:38 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb';
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app'
+import {hash} from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
     beforeAll(async () => {
@@ -8,7 +12,7 @@ describe('Login Routes', () => {
     })
 
     beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('accounts')
+        accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.deleteMany({})
     })
 
@@ -25,6 +29,24 @@ describe('Login Routes', () => {
                 email: 'victor@email.com',
                 password: '123456',
                 passwordConfirmation: '123456'
+            })
+            .expect(200)
+        })
+    })
+
+    describe('POST /login', () => {
+        test('Should return 200 on login', async () => {
+            const password = await hash('123456', 12)
+            await accountCollection.insertOne({
+                name: 'Victor',
+                email: 'victor@email.com',
+                password
+            })
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'victor@email.com',
+                    password: '123456',
             })
             .expect(200)
         })
