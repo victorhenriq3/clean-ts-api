eb4df0bc21c368bfb4fb0915cdddcd377569ad94
victorhenriqc <vic.1601@outlook.com>
2023-03-14 22:21:29 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,27 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from "../helpers/mongo-helper"
 
-export class AccountMongoRepository implements AddAccountRepository{
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository{
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         const result = await accountCollection.insertOne(accountData)
         
         return { ...accountData, id: result.insertedId.toString()}
+    }
+
+    async loadByEmail(email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const account = await accountCollection.findOne({email})
+        // const payload = {
+        //     id: account._id.toString(),
+        //     name: account.name,
+        //     email: account.email,
+        //     password: account.password
+        // }
+
+        return account && MongoHelper.map(account)
     }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -19,5 +19,10 @@ export const MongoHelper = {
             await this.connect(this.uri)
         }
         return this.client.db().collection(name)
+    },
+
+    map: (collection: any): any => {
+        const {_id, ...collectionWithoutId} = collection
+        return Object.assign({}, collectionWithoutId, {id: _id})
     }
 }
