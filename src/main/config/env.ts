0582ceac2be41d06312f0626c24ecbe94dcb9f8a
victorhenriqc <vic.1601@outlook.com>
2023-03-15 14:52:26 -0300

    feat: add loginfactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
     port: process.env.PORT || 5050,
-    salt: 12
+    salt: 12,
+    jwtSecret: process.env.JWT_SECRET || 'jwx212a_1s!',
 }
