20abe8d4fda49021ed6422723abaed8737e637aa
victorhenriq3 <vic.1601@outlook.com>
2023-03-17 11:26:56 -0300

    refactor: change error in survey route

--- src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -8,7 +8,7 @@ describe('AddSurveyValidation Factory', () => {
     test('Should Call validation composite with all validations', () => {
         makeAddSurveyValidation()
         const validations: Validation[] = []
-        for (const field of ['question', 'answer']){
+        for (const field of ['question', 'answers']){
             validations.push( new RequiredFieldValidation(field))
         }
         expect(ValidationComposite).toHaveBeenCalledWith(validations)
--- src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -4,7 +4,7 @@ import { Validation } from '../../../../presentation/protocols/validation'
 
 export const makeAddSurveyValidation= (): ValidationComposite => {
     const validations: Validation[] = []
-    for (const field of ['question', 'answer']){
+    for (const field of ['question', 'answers']){
         validations.push( new RequiredFieldValidation(field))
     }
     return new ValidationComposite(validations)
