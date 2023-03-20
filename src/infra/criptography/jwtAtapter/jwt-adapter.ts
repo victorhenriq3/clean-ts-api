963fc28e10036689d95689e80910a844d32953a7
victorhenriq3 <vic.1601@outlook.com>
2023-03-20 11:31:32 -0300

    refactor: separe test in describes blocks

--- src/infra/criptography/jwtAtapter/jwt-adapter.ts
+++ src/infra/criptography/jwtAtapter/jwt-adapter.ts
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken'
+import { Decrypter } from '../../../data/protocols/criptography/decrypter';
 import { Encrypter } from "../../../data/protocols/criptography/encrypter";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
     private readonly secret: string
     constructor(secret: string){
         this.secret = secret
+    }
+    decrypt(value: string): Promise<string> {
+        throw new Error('Method not implemented.');
     }
     async encrypt(value: string): Promise<string> {
         const accessToken = await jwt.sign({id: value}, this.secret)
