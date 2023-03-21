02106ed0461b6abf8e93abbf6c1f68c27cc84e6c
victorhenriq3 <vic.1601@outlook.com>
2023-03-21 16:24:01 -0300

    refactor: organize surveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -4,6 +4,11 @@ import { SurveyMongoRepository } from './survey-mongo-repository';
 
 let surveyCollection: Collection
 
+
+const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+}
+
 describe('Survey Mongo Repository', () => {
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL);
@@ -18,26 +23,24 @@ describe('Survey Mongo Repository', () => {
         await MongoHelper.disconnect()
     })
 
-    const makeSut = (): SurveyMongoRepository => {
-        return new SurveyMongoRepository()
-    }
-
-    test('Should add a survey on success', async() => {
-        const sut = makeSut()
-        await sut.add({
-            question: 'any_question',
-            answers: [
-                {
-                    image: 'any_image',
-                    answer: 'any_answer'
-                },
-                {
-                    answer: 'other_answer'
-                }
-            ],
-            date: new Date()
+    describe('add()', () => {
+        test('Should add a survey on success', async() => {
+            const sut = makeSut()
+            await sut.add({
+                question: 'any_question',
+                answers: [
+                    {
+                        image: 'any_image',
+                        answer: 'any_answer'
+                    },
+                    {
+                        answer: 'other_answer'
+                    }
+                ],
+                date: new Date()
+            })
+            const survey = await surveyCollection.findOne({ question: 'any_question'})
+            expect(survey).toBeTruthy()
         })
-        const survey = await surveyCollection.findOne({ question: 'any_question'})
-        expect(survey).toBeTruthy()
     })
 })
