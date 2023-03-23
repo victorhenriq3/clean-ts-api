1988fc592fc9f26d013ce996c11afe33f04078a4
victorhenriq3 <vic.1601@outlook.com>
2023-03-23 15:47:07 -0300

    test: ensure Survey Mongo Repository find survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -84,4 +84,29 @@ describe('Survey Mongo Repository', () => {
             expect(surveys.length).toBe(0)
         })
     })
+    
+    describe('loadById()', () => {
+        test('Should load survey by id on success', async() => {
+            const res = await surveyCollection.insertOne(
+                {
+                    question: 'any_question',
+                    answers: [
+                        {
+                            image: 'any_image',
+                            answer: 'any_answer'
+                        },
+                    ],
+                    date: new Date()
+                })
+            const sut = makeSut()
+            const survey = await sut.loadById(String(res.insertedId))
+            expect(survey).toBeTruthy()
+        })
+
+        test('Should load empty list', async() => {
+            const sut = makeSut()
+            const surveys = await sut.loadAll()
+            expect(surveys.length).toBe(0)
+        })
+    })
 })
