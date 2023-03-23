5458bb8d2f4738fab78564dbe16e598df9a5fcf2
victorhenriq3 <vic.1601@outlook.com>
2023-03-23 15:32:18 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct value

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,56 @@
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import MockDate from 'mockdate'
+import { SaveSurveyResultModel, SurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+    accountId: 'any_account_id',
+    surveyId: 'any_survey_id',
+    answer: 'any_answer',
+    date: new Date()
+})
+
+const makeFakeSurveyResult = ():SurveyResultModel => Object.assign({}, makeFakeSurveyResultData(), {
+    id: 'any_id'
+})
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+    class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository{
+        async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+            return new Promise(resolve => resolve(makeFakeSurveyResult()))
+        }
+
+    }
+    return new SaveSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+    sut: DbSaveSurveyResult
+    saveSurveyResultRepository: SaveSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+    const saveSurveyResultRepository = makeSaveSurveyResultRepository()
+    const sut = new DbSaveSurveyResult(saveSurveyResultRepository)
+
+    return {
+        sut,
+        saveSurveyResultRepository
+    }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+    test('Should call SaveSurveyResultRepository with correct values', async () => {
+        const {sut, saveSurveyResultRepository} = makeSut()
+        const saveSpy = jest.spyOn(saveSurveyResultRepository, 'save')
+        const surveyResultData = makeFakeSurveyResultData()
+        await sut.save(surveyResultData)
+        expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
+    })
+})
