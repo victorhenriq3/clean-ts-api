93ea6ed9e491739c8fb0cc8585470411060c03df
victorhenriq3 <vic.1601@outlook.com>
2023-03-23 20:05:22 -0300

    test: ensre SaveSurveyResult return 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,6 +1,6 @@
 
 import { InvalidParamError } from "@/presentation/errors"
-import { forbidden, serverError } from "@/presentation/helpers/http/http-helper"
+import { forbidden, ok, serverError } from "@/presentation/helpers/http/http-helper"
 import { SaveSurveyResultController } from "./save-survey-result-controller"
 import { HttpRequest, LoadSurveyById, SurveyModel, SaveSurveyResultModel, SaveSurveyResult, SurveyResultModel } from "./save-survey-result-controller-protocols"
 import MockDate from 'mockdate'
@@ -132,5 +132,11 @@ describe('SaveSurveyResult Controller', () => {
         jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(serverError(new Error()))
+    })
+
+    test('Should return 200 on success', async () => {
+        const {sut } = makeSut()
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(ok(makeFakeSurveyResult()))
     })
 })
