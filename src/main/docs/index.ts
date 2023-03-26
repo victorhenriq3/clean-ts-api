7595a57192a400e277ce5acd6203746b0de0594b
victorhenriq3 <vic.1601@outlook.com>
2023-03-26 12:29:17 -0300

    feat: add swagger api config

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'Clean node api',
+        description: 'Api do curso do mango para realizar enquetes entre programadores',
+        version: '1.2.0'
+    }
+}
