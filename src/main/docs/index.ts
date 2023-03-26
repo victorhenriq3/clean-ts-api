d2538d46eb329e90693427c65d775e41124c405d
victorhenriq3 <vic.1601@outlook.com>
2023-03-26 16:19:47 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from "./paths/login-path";
+import { accountSchema } from "./schemas/account-schema";
+import { loginParamsSchema } from "./schemas/login-params-schema";
+
 export default {
     openapi: '3.0.0',
     info: {
         title: 'Clean node api',
         description: 'Api do curso do mango para realizar enquetes entre programadores',
-        version: '1.2.0'
+        version: '1.0.0'
+    },
+    servers: [{
+        url: '/api'
+    }],
+    tags: [{
+        name: 'Login'
+    }],
+    paths: {
+        '/login': loginPath
+    },
+    schemas: {
+        account: accountSchema,
+        loginParams: loginParamsSchema
     }
 }
